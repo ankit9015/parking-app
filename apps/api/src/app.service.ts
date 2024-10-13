a3dfb16067d7f14de95f48229d0ae35f11bfebc1
Ankit Joshi <ankitoshi9015@gmail.com>
2024-10-13 18:04:32 +0530

    feat: db created

--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -3,8 +3,6 @@ import { add } from '@parking-app/sample-lib'
 
 @Injectable()
 export class AppService {
-  let a : string;
-  a = 1;
   getHello(): string {
     return 'Hello World! ' + add(34, 3)
   }
