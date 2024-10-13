be4a717d2ff81374d593f09092685e40dddf1b48
Ankit Joshi <ankitoshi9015@gmail.com>
2024-10-13 16:54:13 +0530

    feat: add commit validation

--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -1,8 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { add } from '@parking-app/sample-lib'
 
 @Injectable()
 export class AppService {
+  let a : string;
+  a = 1;
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World! ' + add(34, 3)
   }
 }
